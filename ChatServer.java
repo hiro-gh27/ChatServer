b44bb7a07227cb20a1741ba2dc5b2c624e105321
wazyu <g1344630@cc.kyoto-su.ac.jp>
2015-07-31 13:16:27 +0900

    refs #3 クライアント対応のためのクラスファイルを作成する
    import文が増えてます。
    ChatServerの方も変更ありです。

--- ChatServer.java
+++ ChatServer.java
@@ -1,12 +1,24 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class ChatServer{
     private ServerSocket server;
+    private List ClientList = new LinkedList();
+    
     public void listen(){	
 	try{
+	    int count = 1; //クライアントのカウント用変数
+
 	    server = new ServerSocket(18080);
 	    System.out.println("ChatServerをポート18080で起動しました");
+
+	    Socket socket = server.accept(); //接続要求の待機
+	    System.out.println("クライアント"+count+"が接続してきました"); //接続されるとサーバー側に表示される
+	    ChatClientHandler handler = new ChatClientHandler(socket, ClientList, "undefined"+count); //コンストラクタの呼び出し
+	    ClientList.add(handler);//handlerをクライアントリストに加える
+
+	    count++;
 	}catch(IOException e){
 	    e.printStackTrace();
 	}
