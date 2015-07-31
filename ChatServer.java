4cb0dcd648a00ccd6f02a386c46e67356619c2fd
wazyu <g1344630@cc.kyoto-su.ac.jp>
2015-07-31 12:59:13 +0900

    refs #2 サーバーを立てる
    import java.io.*も追加しました。
    18080番ポートでサーバーを起動する処理追加しました。

--- ChatServer.java
+++ ChatServer.java
@@ -1,9 +1,19 @@
 import java.net.*;
+import java.io.*;
 
 public class ChatServer{
-
-
+    private ServerSocket server;
+    public void listen(){	
+	try{
+	    server = new ServerSocket(18080);
+	    System.out.println("ChatServerをポート18080で起動しました");
+	}catch(IOException e){
+	    e.printStackTrace();
+	}
+    }
+    
     public static void main(String[] args){
         ChatServer chat = new ChatServer();
+	chat.listen();
     }
 }
